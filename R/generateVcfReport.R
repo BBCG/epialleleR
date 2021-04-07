55c02fa2547eab78fb95d346ff64e52349afce17
Oleksii.Nikolaienko <oleksii.nikolaienko@uib.no>
2021-04-07 16:11:43 +0200

    updated description

--- R/generateVcfReport.R
+++ R/generateVcfReport.R
@@ -1,16 +1,37 @@
 #' generateVcfReport
 #'
 #' @description
-#' `generateVcfReport` desc. Epiallele-aware. VCF...
+#' This function calculates base frequencies at particular genomic positions and
+#' tests their association with the methylation status of the sequencing reads.
 #'
 #' @details
-#' details
+#' Using BAM reads and sequence variation information as an input,
+#' `generateVcfReport` function thresholds the reads according to supplied
+#' parameters and calculates the occurence of \strong{Ref}erence and
+#' \strong{Alt}ernative bases withing reads, taking into the account DNA strand
+#' the read mapped to and average methylation level (epiallele status) of the
+#' read.
+#' 
+#' The information on sequence variation can be supplied as a Variant Call
+#' Format (VCF) file location or an object of class VCF, returned by the
+#' \code{\link[VariantAnnotation]{readVcf}} function call. As whole-genome VCF
+#' files can be extremely large, it is strongly advised to use only relevant
+#' subset of their data, prefiltering the VCF object manually before calling
+#' `generateVcfReport` or specifying `bed` parameter when `vcf` points to the
+#' location of such large VCF file. Please note that all the BAM, BED and VCF
+#' files must use the same seqlevels (i.e. chromosome names).
+#' 
+#' After counting, function checks if certain bases occur more often within
+#' reads belonging to certain epialleles using Fisher's Exact test
+#' (\code{\link[stats]{fisher.test}}) and reports separate p-values for reads
+#' mapped to \strong{"+"} (forward) and \strong{"-"} (reverse) DNA strands.
 #'
 #' @param bam BAM file location string OR preprocessed output of
-#' \code{\link{preprocessBam}} function
+#' \code{\link{preprocessBam}} function.
 #' @param vcf Variant Call Format (VCF) file location string OR a VCF object
-#' returned by \code{\link[VariantAnnotation]{readVcf}}. If VCF object is
-#' supplied, its seqlevels must match the seqlevels of the BAM file/object used
+#' returned by \code{\link[VariantAnnotation]{readVcf}} function. If VCF object
+#' is supplied, its seqlevels must match the seqlevels of the BAM file/object
+#' used.
 #' @param vcf.style string for the seqlevels style of the VCF file, if
 #' different from BED file/object. Only has effect when `vcf` param points to
 #' the VCF file location and `bed` is not NULL. Possible values:
@@ -25,20 +46,20 @@
 #' regions of interest. It is used to include only the specific genomic ranges
 #' when the VCF file is loaded. This option has no effect when VCF object is
 #' supplied as a `vcf` param. The seqlevels of BED file/object must match the
-#' seqlevels of the BAM file/object used
+#' seqlevels of the BAM file/object used.
 #' @param report.file file location string to write the VCF report. If NULL
 #' (the default) then report is returned as a
-#' \code{\link[data.table]{data.table}} object
+#' \code{\link[data.table]{data.table}} object.
 #' @param zero.based.bed boolean defining if BED coordinates are zero based
-#' (default: FALSE)
+#' (default: FALSE).
 #' @param threshold.reads boolean defining if sequence reads should be
 #' thresholded before counting bases in reference and variant epialleles
 #' (default: TRUE). Disabling thresholding is possible but makes no sense in
 #' this context, as all the reads will be assigned to the variant epiallele,
 #' which will result in Fisher's Exact test p-value of 1 (in columns `FEp+` and
-#' `FEP-`)
+#' `FEP-`).
 #' @param threshold.context string defining cytosine methylation context used
-#' for thresholdning the reads
+#' for thresholdning the reads:
 #' \itemize{
 #'   \item "CG" (the default) -- within-the-context: CpG cytosines (called as
 #'   zZ), out-of-context: all the other cytosines (hHxX)
@@ -49,30 +70,31 @@
 #'   \item "CX" -- all cytosines are considered within-the-context, this
 #'   effectively results in no thresholdning
 #' }
-#' This option has no effect when read thresholding is disabled
+#' This option has no effect when read thresholding is disabled.
 #' @param min.context.sites non-negative integer for minimum number of cytosines
 #' within the `threshold.context` (default: 2). Reads containing \strong{fewer}
 #' within-the-context cytosines are considered completely unmethylated (thus
 #' belonging to the reference epiallele). This option has no effect when read
-#' thresholding is disabled
+#' thresholding is disabled.
 #' @param min.context.beta real number in the range [0;1] (default: 0.5). Reads
 #' with average beta value for within-the-context cytosines \strong{below} this
 #' threshold are considered completely unmethylated (thus belonging to the
 #' reference epiallele). This option has no effect when read thresholding is
-#' disabled
+#' disabled.
 #' @param max.outofcontext.beta real number in the range [0;1] (default: 0.1).
 #' Reads with average beta value for out-of-context cytosines \strong{above}
 #' this threshold are considered completely unmethylated (thus belonging to the
 #' reference epiallele). This option has no effect when read thresholding is
-#' disabled
+#' disabled.
 #' @param min.mapq non-negative integer threshold for minimum read mapping
 #' quality (default: 0). Option has no effect if preprocessed BAM data was
-#' supplied as an input
+#' supplied as an input.
 #' @param skip.duplicates boolean defining if duplicate aligned reads should be
 #' skipped (default: FALSE). Option has no effect if preprocessed BAM data was
-#' supplied as an input OR duplicate reads were not marked by alignment software
-#' @param gzip boolean to compress the report (default: FALSE)
-#' @param verbose boolean to report progress and timings (default: TRUE)
+#' supplied as an input OR duplicate reads were not marked by alignment
+#' software.
+#' @param gzip boolean to compress the report (default: FALSE).
+#' @param verbose boolean to report progress and timings (default: TRUE).
 #' @return \code{\link[data.table]{data.table}} object containing VCF report or
 #' NULL if report.file was specified. The report columns are:
 #' \itemize{
@@ -83,23 +105,22 @@
 #'   \item ALT -- base at the alternative allele
 #'   \item [M|U][+|-][Ref|Alt] -- number of \strong{Ref}erence or
 #'   \strong{Alt}ernative bases that were found at this particular position
-#'   within \strong{M}ethylated or \strong{U}nmethylated reads that were mapped
-#'   to "\strong{+}" (forward) or "\strong{-}" (reverse) DNA strand
+#'   within \strong{M}ethylated (above threshold) or \strong{U}nmethylated
+#'   (below threshold) reads that were mapped to \strong{"+"} (forward) or
+#'   \strong{"-"} (reverse) DNA strand
 #'   \item SumRef -- sum of all \strong{Ref}erence base counts
 #'   \item SumAlt -- sum of all \strong{Alt}ernative base counts
 #'   \item FEp+ -- Fisher's Exact test p-value for association of a variation
-#'   with methylation status of the reads that map to the "\strong{+}" (forward)
+#'   with methylation status of the reads that map to the \strong{"+"} (forward)
 #'   DNA strand. Calculated using following contingency table:
-#'   
 #'   \tabular{ll}{
 #'     M+Ref \tab M+Alt \cr
 #'     U+Ref \tab U+Alt 
 #'   }
 #'   
 #'   \item FEp- -- Fisher's Exact test p-value for association of a variation
-#'   with methylation status of the reads that map to the "\strong{-}" (reverse)
+#'   with methylation status of the reads that map to the \strong{"-"} (reverse)
 #'   DNA strand. Calculated using following contingency table:
-#'   
 #'   \tabular{ll}{
 #'     M-Ref \tab M-Alt \cr
 #'     U-Ref \tab U-Alt 
@@ -109,7 +130,12 @@
 #' \code{\link{generateCytosineReport}} for methylation statistics at the level
 #' of individual cytosines, \code{\link{generateBedReport}} for genomic
 #' region-based statistics, and `epialleleR` vignettes for the description of
-#' usage and sample data
+#' usage and sample data.
+#' 
+#' \code{\link[GenomicRanges]{GRanges}} class for working with genomic ranges,
+#' \code{\link[VariantAnnotation]{readVcf}} function for loading VCF data,
+#' \code{\link[GenomeInfoDb]{seqlevelsStyle}} function for getting or setting
+#' the seqlevels style.
 #' @examples
 #'   capture.bam <- system.file("extdata", "capture.bam", package="epialleleR")
 #'   capture.bed <- system.file("extdata", "capture.bed", package="epialleleR")
