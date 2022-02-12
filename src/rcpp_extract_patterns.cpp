63f270bdc7cd830413785b08df8c662ef1625ed3
Oleksii.Nikolaienko <oleksii.nikolaienko@uib.no>
2022-02-12 16:11:10 +0100

    patterns on their way

---
+++ src/rcpp_extract_patterns.cpp
@@ -0,0 +1,84 @@
+#include <Rcpp.h>
+// using namespace Rcpp;
+
+// Scans trough reads and extracts methylation patterns from the reads that
+// overlap target area. Clips overhangs if necessary.
+// Return value: a data frame with the following columns:
+// 1) pattern id (FNV hash)
+// 1) 
+
+// fast, vectorised
+// [[Rcpp::export("rcpp_extract_patterns")]]
+Rcpp::DataFrame rcpp_extract_patterns(Rcpp::DataFrame &df,                      // data frame with BAM data
+                                      int target_rname,                         // 
+                                      int target_start,                         // 
+                                      int target_end,                           // 
+                                      signed int min_overlap,                   // min overlap of reads and capture targets
+                                      std::string &ctx,                         // context string for bases to include
+                                      bool clip) {                              // clip the matched reads to target area
+  Rcpp::IntegerVector rname   = df["rname"];                                    // template rname
+  Rcpp::IntegerVector strand  = df["strand"];                                   // template strand
+  Rcpp::IntegerVector start   = df["start"];                                    // template start
+  Rcpp::IntegerVector templid = df["templid"];                                  // template id, effectively holds indexes of corresponding std::string in std::vector
+  
+  Rcpp::XPtr<std::vector<std::string>> xm((SEXP)df.attr("xm_xptr"));            // merged refspaced template XMs, as a pointer to std::vector<std::string>
+  
+  std::map<int, std::vector<int>> pat;                                          // per-pattern counts
+  std::vector<uint64_t> patid;                                                  // FNV hashes of patterns
+  
+  // // rnames are sorted, should've been taking an advantage of it...
+  // Rcpp::IntegerVector::const_iterator lower = std::lower_bound(rname.begin(), rname.end(), target_rname);
+  // Rcpp::IntegerVector::const_iterator upper = std::upper_bound(lower, rname.end(), target_rname);
+  // for (unsigned int x=lower-rname.begin(); x<upper-rname.begin(); x++) {
+  
+  for (unsigned int x=0; x<rname.size(); x++) {   
+    // checking for the interrupt
+    if ((x & 0xFFFFF) == 0) Rcpp::checkUserInterrupt();
+    
+    if (rname[x]==target_rname) {
+      const unsigned int size_x = xm->at(templid[x]).size();                    // length of the current read
+      const unsigned int end_x = start[x] + size_x - 1;
+      signed int overlap = std::min(end_x, target_end) - std::max(start[x], target_start) + 1;
+      if (overlap>=min_overlap) {
+        const char* xm_x = xm->at(templid[x]).c_str();                          // xm->at(templid[x]) is a reference to a corresponding XM string
+        for (unsigned int i=0; i<size_x; i++) {                                 // char by char - it's faster this way than using std::string in the cycle
+          
+        }
+      }
+    }
+
+  }
+  
+  
+  std::vector<int> x1(4,6) ;
+  std::vector<int> x2(4,2) ;
+  std::vector<int> x3(4,3) ;
+  
+  std::map< int, std::vector<int> > m ;
+  m.emplace(11, x1);
+  m.emplace(44, x2);
+  m.emplace(33, x3);
+  
+  return Rcpp::wrap( m ) ;
+}
+
+
+
+
+
+
+
+
+
+
+// test code in R
+//
+
+/*** R
+rcpp_extract_patterns(bam, 47, 43124861, 43125261, 1, "zZ", TRUE)
+*/
+
+// Sourcing:
+// Rcpp::sourceCpp("rcpp_cx_report.cpp")
+
+// #############################################################################
