a538922251aa4eae6604004944a0790e36180f66
Oleksii.Nikolaienko <oleksii.nikolaienko@uib.no>
2022-02-12 17:20:42 +0100

    patterns on their way

--- src/rcpp_extract_patterns.cpp
+++ src/rcpp_extract_patterns.cpp
@@ -15,7 +15,8 @@ Rcpp::DataFrame rcpp_extract_patterns(Rcpp::DataFrame &df,                      
                                       int target_end,                           // 
                                       signed int min_overlap,                   // min overlap of reads and capture targets
                                       std::string &ctx,                         // context string for bases to include
-                                      bool clip) {                              // clip the matched reads to target area
+                                      bool clip,                                // clip the matched reads to target area
+                                      unsigned int reverse_offset) {            // decrease reverse strand coordinates by this value: 0 for CHH, 1 for CpG, 2 for CHG
   Rcpp::IntegerVector rname   = df["rname"];                                    // template rname
   Rcpp::IntegerVector strand  = df["strand"];                                   // template strand
   Rcpp::IntegerVector start   = df["start"];                                    // template start
@@ -37,12 +38,18 @@ Rcpp::DataFrame rcpp_extract_patterns(Rcpp::DataFrame &df,                      
     
     if (rname[x]==target_rname) {
       const unsigned int size_x = xm->at(templid[x]).size();                    // length of the current read
-      const unsigned int end_x = start[x] + size_x - 1;
-      signed int overlap = std::min(end_x, target_end) - std::max(start[x], target_start) + 1;
-      if (overlap>=min_overlap) {
+      const unsigned int start_x = start[x];                                    // start position of the current read
+      const unsigned int end_x = start_x + size_x - 1;                          // end position of the current read
+      const signed int overlap = std::min(end_x, target_end) - std::max(start_x, target_start) + 1; // overlap with target
+      if (overlap>=min_overlap) {                                               // if overlaps the target
         const char* xm_x = xm->at(templid[x]).c_str();                          // xm->at(templid[x]) is a reference to a corresponding XM string
-        for (unsigned int i=0; i<size_x; i++) {                                 // char by char - it's faster this way than using std::string in the cycle
-          
+        const unsigned int offset_x = strand[x]==2 ? reverse_offset : 0;        // offset coordinates of reverse strand for symmetric methylation
+        const unsigned int begin_i = clip ? (start_x - std::max(start_x, target_start)) : 0;
+        const unsigned int end_i = clip ? overlap : size_x;                     // ^ clip the XM?
+        for (unsigned int i=begin_i; i<end_i; i++) {                            // char by char - it's faster this way than using std::string in the cycle
+          if (ctx.find(xm_x[i]) != std::string::npos) {
+            const unsigned int pos = start_x + i - offset_x;
+          }
         }
       }
     }
@@ -75,7 +82,7 @@ Rcpp::DataFrame rcpp_extract_patterns(Rcpp::DataFrame &df,                      
 //
 
 /*** R
-rcpp_extract_patterns(bam, 47, 43124861, 43125261, 1, "zZ", TRUE)
+rcpp_extract_patterns(bam, 47, 43124861, 43125261, 1, "zZ", TRUE, 1)
 */
 
 // Sourcing:
