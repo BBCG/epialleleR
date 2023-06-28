2afa46da8d57972382cb0a681d6a6f0a8fbf60f9
Oleksii.Nikolaienko <oleksii.nikolaienko@uib.no>
2023-06-28 23:28:16 +0200

    calls on the way

--- src/rcpp_call_methylation.cpp
+++ src/rcpp_call_methylation.cpp
@@ -27,7 +27,7 @@ Rcpp::List rcpp_call_methylation_genome (std::string in_fn,                     
   htsThreadPool thread_pool = {NULL, 0};                                        // thread pool cuts time by 30%
   if (nthreads>0) {
     thread_pool.pool = hts_tpool_init(nthreads);                                // when initiated for >0 threads
-    hts_set_opt(in_fp, HTS_OPT_THREAD_POOL, &thread_pool);                      // and bound to the input
+    hts_set_opt(in_fp,  HTS_OPT_THREAD_POOL, &thread_pool);                     // and bound to the input
     hts_set_opt(out_fp, HTS_OPT_THREAD_POOL, &thread_pool);                     // and output file pointers
   }
   // https://github.com/samtools/htslib/blob/e86126bc45f6cf6b1c3d67e13de96aeaa5e58805/test/test_view.c#L389
@@ -40,12 +40,23 @@ Rcpp::List rcpp_call_methylation_genome (std::string in_fn,                     
   
   // vars
   int nrecs = 0;                                                                // counters: BAM records
+  int max_query_width = 1024;                                                   // max query width, expanded if necessary
+  int query_width = max_query_width;                                            // query width
+  uint8_t *xm = (uint8_t*) malloc(max_query_width * sizeof(uint8_t));           // XM array
   
   while(sam_read1(in_fp, in_hdr, in_rec) > 0) {                                 // read rec by rec
     nrecs++;                                                                    // BAM alignment records ++
     if ((nrecs & 0xFFFFF) == 0) Rcpp::checkUserInterrupt();                     // every ~1M reads check for the interrupt
     
+    query_width = abs(in_rec->core.l_qseq);                                     // query width
+    if (query_width > max_query_width) {                                        // if sequence is longer than XM holder
+      max_query_width = query_width;                                            // new max
+      xm = (uint8_t *) realloc(xm, max_query_width);                            // expand xm holder
+      if (xm==NULL) Rcpp::stop("No memory for BAM record #%i", nrecs);          // check memory allocation
+    }
+    
     // meth calling
+    
     
     
     if (sam_write1(out_fp, in_hdr, in_rec) < 0) Rcpp::stop("Unable to write BAM"); // write rec
@@ -56,6 +67,7 @@ Rcpp::List rcpp_call_methylation_genome (std::string in_fn,                     
   hts_close(in_fp);                                                             // close input BAM file
   hts_close(out_fp);                                                            // close output BAM file
   if (thread_pool.pool) hts_tpool_destroy(thread_pool.pool);                    // free thread pool
+  free(xm);                                                                     // and free manually allocated memory
   
   // wrap and return the results
   Rcpp::List res = Rcpp::List::create(                                          // final List
