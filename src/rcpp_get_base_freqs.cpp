946502379eb83fa95cbfb61efcf05c3087ae5b01
Oleksii.Nikolaienko <oleksii.nikolaienko@uib.no>
2024-03-21 13:14:29 +0100

    +get_base_freqs

--- src/rcpp_get_base_freqs.cpp
+++ src/rcpp_get_base_freqs.cpp
@@ -1,18 +1,13 @@
 #include <Rcpp.h>
+#include <htslib/hts.h>
 // using namespace Rcpp;
 
 // Matches reads with given 1-base positions (VCF) and returns base frequencies.
 // FUNCTION ASSUMES THAT BOTH READS AND VCF ENTRIES ARE SORTED!
-// Return value: std::array<int,32> for each position in VCF
-// Array indices are calculated as (char&7 | pass?15:0 | strand?31:0),
-// thus they appear as following: U+*8, U-*8, M+*8, M-*8
-//
-// char  &7   dec0b
-// Aa    001  1
-// Cc    011  3
-// Gg    111  7
-// Nn    110  6
-// Tt    100  4
+// Return value: std::array<int,20> for each position in VCF
+// Array indices are 0123 for ACGT, and 4 for N and extended IUPAC,
+// thus they appear as following: U+*5, U-*5, M+*5, M-*5
+
 
 // MATCH VCF ENTRIES, RETURN BASE FREQS
 // fast, vectorised
@@ -24,36 +19,39 @@ Rcpp::NumericMatrix rcpp_get_base_freqs(Rcpp::DataFrame &df,                    
   Rcpp::IntegerVector read_rname = df["rname"];                                 // template rname
   Rcpp::IntegerVector read_strand = df["strand"];                               // template strand
   Rcpp::IntegerVector read_start = df["start"];                                 // template start
-  // Rcpp::CharacterVector read_seq = bam["seq"];                                  // template SEQ
-  Rcpp::XPtr<std::vector<std::string>> read_seq((SEXP)df.attr("seq_xptr"));     // merged refspaced template SEQ, as a pointer to std::vector<std::string>
+  Rcpp::XPtr<std::vector<std::string>> seqxm((SEXP)df.attr("seqxm_xptr"));      // merged refspaced packed template SEQXMs, as a pointer to std::vector<std::string>
   Rcpp::IntegerVector templid = df["templid"];                                  // template id, effectively holds indexes of corresponding std::string in std::vector
   
   Rcpp::IntegerVector vcf_chr = vcf["seqnames"];                                // VCF rname
   Rcpp::IntegerVector vcf_pos = vcf["start"];                                   // VCF start
   
-  Rcpp::NumericMatrix res(vcf_pos.size(),32);
+  Rcpp::NumericMatrix res(vcf_pos.size(),20);
   
   int cur_vcf=0;
   for (unsigned int x=0; x<read_start.size(); x++) {
     // checking for the interrupt
     if ((x & 0xFFFFF) == 0) Rcpp::checkUserInterrupt();
     
-    int read_end = read_start[x] + read_seq->at(templid[x]).size() - 1;
+    const int read_rname_x = read_rname[x];
+    const int read_start_x = read_start[x];
+    const int read_end_x = read_start_x + seqxm->at(templid[x]).size() - 1;
     for (unsigned int i=cur_vcf; i<vcf_pos.size(); i++) {
-      if (vcf_chr[i]<read_rname[x] || 
-          (vcf_chr[i]==read_rname[x] && vcf_pos[i]<read_start[x])) {            // skip VCF if before read
+      const int vcf_chr_i = vcf_chr[i];
+      const int vcf_pos_i = vcf_pos[i];
+      if (vcf_chr_i<read_rname_x || 
+          (vcf_chr_i==read_rname_x && vcf_pos_i<read_start_x)) {                // skip VCF if before read
         cur_vcf=i;
         continue;
       }
-      if (vcf_chr[i]>read_rname[x] ||
-          (vcf_chr[i]==read_rname[x] && vcf_pos[i]>read_end)) {                 // start over from cur_vcf for new read
+      if (vcf_chr_i>read_rname_x ||
+          (vcf_chr_i==read_rname_x && vcf_pos_i>read_end_x)) {                  // start over from cur_vcf for new read
         break;
       }
-      if (vcf_chr[i]==read_rname[x] &&
-          vcf_pos[i]>=read_start[x] && vcf_pos[i]<=read_end) {                  // match found
-        int idx = read_seq->at(templid[x])[vcf_pos[i]-read_start[x]] & 7;
-        if (read_strand[x]==2) idx |= 8;
-        if (pass[x]) idx |= 16;
+      if (vcf_chr_i==read_rname_x &&
+          vcf_pos_i>=read_start_x && vcf_pos_i<=read_end_x) {                   // match found
+        int idx = seq_nt16_int[(seqxm->at(templid[x])[vcf_pos_i-read_start_x]) >> 4]; // index of a base, [0;4]
+        idx += (read_strand[x]-1) * 5;                                          // shift by 5 if '-' strand (==2)
+        idx += ((bool)(pass[x])) * 10;                                          // shift by 10 if pass==TRUE (==1)
         res(i,idx)++;
       }
     }
